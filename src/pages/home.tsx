a05d1dde3d2ee3fc0ffc9dce2ed95228444a5a73
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2025-08-22 01:19:05 +0700

    Redesigned the Home and Settings Pt. 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { ArrowRight, ClipboardIcon, PlusCircle, Search } from "lucide-react";
+import { ClipboardIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
@@ -37,10 +37,16 @@ function App() {
 
   return (
     <div className="flex flex-col gap-3 items-center justify-center h-svh">
-      <div className="absolute top-2 right-0 left-0 flex flex-row justify-center items-center p-2">
+      <div className="absolute top-0 right-0 left-0 grid grid-cols-3 items-center p-2">
+        <Link to="/settings" className="w-fit mr-auto">
+          <Button variant="outline">
+            <Settings />
+            Settings
+          </Button>
+        </Link>
         <Popover>
           <PopoverTrigger asChild className="z-50">
-            <Button variant="outline">
+            <Button variant="outline" className="w-fit mx-auto">
               <PlusCircle />
               Add to Browser
             </Button>
@@ -74,9 +80,18 @@ function App() {
             </div>
           </PopoverContent>
         </Popover>
+        <div className="flex flex-row gap-2 ml-auto">
+          <Button variant="outline" size="icon" onClick={handleXIydheko}>
+            <svg viewBox="0 0 256 209" width="256" height="209" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M256 25.45c-9.42 4.177-19.542 7-30.166 8.27 10.845-6.5 19.172-16.793 23.093-29.057a105.183 105.183 0 0 1-33.351 12.745C205.995 7.201 192.346.822 177.239.822c-29.006 0-52.523 23.516-52.523 52.52 0 4.117.465 8.125 1.36 11.97-43.65-2.191-82.35-23.1-108.255-54.876-4.52 7.757-7.11 16.78-7.11 26.404 0 18.222 9.273 34.297 23.365 43.716a52.312 52.312 0 0 1-23.79-6.57c-.003.22-.003.44-.003.661 0 25.447 18.104 46.675 42.13 51.5a52.592 52.592 0 0 1-23.718.9c6.683 20.866 26.08 36.05 49.062 36.475-17.975 14.086-40.622 22.483-65.228 22.483-4.24 0-8.42-.249-12.529-.734 23.243 14.902 50.85 23.597 80.51 23.597 96.607 0 149.434-80.031 149.434-149.435 0-2.278-.05-4.543-.152-6.795A106.748 106.748 0 0 0 256 25.45" fill="#55acee"/></svg>
+          </Button>
+          <Button variant="outline">
+            <svg width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)" fill="#ffff"/></svg>
+            GitHub
+          </Button>
+        </div>
       </div>
       <div className="max-w-xl space-y-4">
-        <h1 className="md:w-sm md:text-6xl leading-18 text-2xl mb-12 font-bold">cuzbangs. cuz it bangs.</h1>
+        <h1 className="md:w-sm md:text-6xl leading-18 text-2xl mb-8 font-bold">cuzbangs. cuz it bangs.</h1>
         <form 
           action="https://cuzbangs.vercel.app/go" 
           method="get" 
@@ -104,14 +119,14 @@ function App() {
         </form>
         <div className="flex flex-row justify-center">
           <Button variant="link" onClick={handleGitHubClick}>
-            GitHub
+            How cuzbangs work?
           </Button>
           <Button variant="link" onClick={handleXIydheko}>
-            Ferdinan Iydheko
+            Bangs Lists
           </Button>
-          <Link to="/settings">
-            <Button variant="link">Settings</Button>
-          </Link>
+          <Button variant="link" onClick={handleXIydheko}>
+            Changelogs
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { LucideArrowLeft } from "lucide-react";
+import { ChevronsUpDown, LucideArrowLeft, PlusCircle, Trash } from "lucide-react";
 import { Link } from "react-router-dom";
 import Yeah from "../components/Yeah";
 import DefaultSearchEngine from "./settings/DefaultSearchEngine";
@@ -10,7 +10,22 @@ import CustomBangs from "./settings/CustomBangs";
 import { useSettings } from "../context/SettingsContext";
 import ImportExport from "./settings/ImportExport";
 import { useEffect } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
 
 export default function Settings() {
 
@@ -27,8 +42,8 @@ export default function Settings() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-      <ScrollArea className="h-screen w-full px-4 relative">
-        <div className="grid grid-cols-3 items-center border rounded-lg p-2 absolute top-4 bg-background/40 backdrop-blur-sm z-10">
+      <div className="h-screen w-full grid">
+        <div className="grid grid-cols-3 items-center border-b p-2 top-0 left-0 right-0 bg-background/40 backdrop-blur-sm z-10">
           <Link to="/">
             <Button variant="outline">
               <LucideArrowLeft />
@@ -38,7 +53,7 @@ export default function Settings() {
           <h1 className="text-xl font-bold text-center">Settings</h1>
           <Yeah />
         </div>
-        <div className="flex flex-col gap-4 mt-7">
+        <div className="flex flex-col gap-4 p-4 h-full overflow-y-auto">
           <ImportExport />
           <DefaultSearchEngine />
           <SymbolCall
@@ -56,9 +71,77 @@ export default function Settings() {
             Copyright &copy; 2025 Ferdinan Iydheko
           </h1>
         </div>
-      </ScrollArea>
-      <div className="md:border-l h-screen p-4 pt-6">
-        <h1 className="text-2xl font-bold">Bangs</h1>
+      </div>
+      <div className="h-screen w-full flex flex-col md:border-l">
+        <div className="h-fit flex flex-row justify-between items-center border-b p-2 top-0 left-0 right-0 bg-background/40 backdrop-blur-sm z-10">
+          <h1 className="text-xl font-bold text-center ms-2">Bangs</h1>
+          <Button variant="outline" className="w-fit ml-auto"><PlusCircle /> Add bangs</Button>
+        </div>
+        <div className="flex flex-col gap-4 p-4 h-full overflow-y-auto">
+          <Collapsible>
+            <Card>
+              <CardHeader className="flex flex-row items-center gap-4">
+                <div className="w-10 h-10 bg-neutral-900 rounded-md">
+
+                </div>
+                <CardTitle className="flex-1 text-lg">Google</CardTitle>
+                <CardAction>
+                <CollapsibleTrigger>
+                  <Button variant="ghost" size="icon">
+                  <ChevronsUpDown />
+                  </Button>
+                  </CollapsibleTrigger>
+                </CardAction>
+              </CardHeader>
+              <CollapsibleContent>
+                <CardContent className="space-y-4">
+                  <div className="grid w-full items-center gap-1.5 mt-2">
+                    <Label htmlFor="customurl">Bangs name</Label>
+                    <Input
+                      type="text"
+                      id="customurl"
+                      placeholder="Bangs name"
+                    />
+                  </div>
+                  <div className="grid w-full items-center gap-1.5 mt-2">
+                    <Label htmlFor="customurl">Bangs call</Label>
+                    <Input
+                      type="text"
+                      id="customurl"
+                      placeholder="Bangs call"
+                    />
+                  </div>
+                  <div className="grid w-full items-center gap-1.5 mt-2">
+                    <Label htmlFor="customurl">Bangs URL</Label>
+                    <Input
+                      type="text"
+                      id="customurl"
+                      placeholder="Bangs URL"
+                    />
+                  </div>
+                  <div className="flex flex-row justify-between items-center">
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <div className="flex items-center space-x-2">
+                          <Checkbox
+                            id="noquery"
+                            
+                          />
+                          <Label className="pt-1" htmlFor="noquery">Set Just for Calling</Label>
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Set this bangs just for redirect.</p>
+                        <p>It won't send any queries to this bangs.</p>
+                      </TooltipContent>
+                    </Tooltip>
+                    <Button variant="destructive"><Trash /> Delete</Button>
+                  </div>
+                </CardContent>
+              </CollapsibleContent>
+            </Card>
+          </Collapsible>
+        </div>
       </div>
     </div>
   );
